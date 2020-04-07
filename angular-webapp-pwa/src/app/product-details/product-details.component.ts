be99cde259a397ee1c899ad6c986b29611d294ea
Sai <saisunder12345@gmail.com>
2020-04-08 01:43:08 +0530

    login-feature: Implementation of Google SignIn

--- angular-webapp-pwa/src/app/product-details/product-details.component.ts
+++ angular-webapp-pwa/src/app/product-details/product-details.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { AuthService, SocialUser } from 'angularx-social-login';
 import { products } from '../products';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -10,14 +11,24 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product;
-
+  user: SocialUser;
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
-      this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')];
+
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      if (this.user) {
+        this.route.paramMap.subscribe(params => {
+          this.product = products[+params.get('productId')];
+        });
+      } else {
+        this.router.navigate(['/login']);
+      }
     });
   }
 
